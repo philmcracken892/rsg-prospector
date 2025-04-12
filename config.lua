6b906e1f25d687198a920fc2e6b290e14aa5774e
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-04-12 14:58:18 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,16 @@
 Config = {}
 
+Config.HoleDistance = 2.0 -- Distance to prevent digging near existing dirt piles
 
 Config.Dig = {
     shovel = "p_shovel02x",
     anim = {"amb_work@world_human_gravedig@working@male_b@idle_a", "idle_a"},
     bone = "skel_r_hand",
     pos = {0.06, -0.06, -0.03, 270.0, 165.0, 151.0},
-    duration = 15000 -- Duration in milliseconds (15 seconds)
+    duration = 15000, -- Duration in milliseconds (15 seconds)
+    dirtModel = 'mp005_p_dirtpile_tall_unburied', -- Dirt pile model
+    dirtPileDuration = 300000 -- Duration in milliseconds (5 minutes)
 }
-
 
 Config.LootTable = {
     { item = 'goldbar', chance = 10, amount = 1 },
